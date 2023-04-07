9b210441295c952c0e3405df8bdeb8b79efa18e6
tiyago1 <tiyago1@gmail.com>
2023-04-07 23:10:40 +0300

    Player Movement

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MovementController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 5f;
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0f, 0);
+        movementDirection.Normalize();
+
+        Vector3 newPosition = transform.forward * movementSpeed * Time.deltaTime;
+
+        transform.position += newPosition;
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
