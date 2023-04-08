1a7048620e50993a3c9498c431e4b00057ec7930
tiyago1 <tiyago1@gmail.com>
2023-04-08 10:48:09 +0300

    snake mechanic added

---
+++ Assets/Scripts/BodyPartItem.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BodyPartItem : MonoBehaviour
+    {
+        [SerializeField] private Character holdingCharacter;
+        private BodyPartItem _connectedBodyPartItem;
+        private Tween _moveTween;
+        private Tween _lookTween;
+
+        [SerializeField] private float speed;
+        [SerializeField] private float addInterval;
+        [SerializeField] private float threshold = .1f;
+        
+        private Vector3 _currentTarget;
+
+        private List<Vector3> points;
+        public Queue<Vector3> lastPoints;
+        private float timer;
+        private int seconds;
+        private bool _isConnected;
+
+        public void Initialize(Character character, BodyPartItem connectedBodyPart)
+        {
+            holdingCharacter = character;
+            _connectedBodyPartItem = connectedBodyPart;
+            _currentTarget = Vector3.one * -1;
+            points = new List<Vector3>();
+            lastPoints = new Queue<Vector3>();
+            this.gameObject.SetActive(true);
+            _connectedBodyPartItem?.SetConnected(true);
+        }
+
+        private void Update()
+        {
+            if (_connectedBodyPartItem == null)
+            {
+                return;
+            }
+
+            if (_lookTween == null)
+            {
+                _lookTween = this.transform.DOLookAt(_connectedBodyPartItem.transform.position, .1f).OnComplete(() => { _lookTween = null; });
+            }
+        }
+        
+        private void UpdatePointList()
+        {
+            lastPoints.Enqueue(this.transform.position);
+            points.Add(this.transform.position);
+        }
+
+        public void SetConnected(bool isConnect)
+        {
+            _isConnected = isConnect;
+        }
+        
+        private void LateUpdate()
+        {
+            timer += Time.deltaTime;
+            seconds = (int) (timer % 60);
+
+            if (_isConnected && timer >= addInterval)
+            {
+                UpdatePointList();
+                timer = 0;
+            }
+            Move();
+        }
+
+        private void Move()
+        {
+            if (_connectedBodyPartItem == null)
+            {
+                return;
+            }
+
+            if (_currentTarget == Vector3.one * -1)
+            {
+                _currentTarget = _connectedBodyPartItem.lastPoints.Dequeue();
+            }
+
+            Vector3 direction = (_currentTarget - transform.position).normalized;
+            this.transform.position += direction * speed * Time.deltaTime;
+            float distance = Vector3.Distance(this.transform.position, _currentTarget);
+
+            if (distance <= threshold)
+            {
+                _currentTarget = _connectedBodyPartItem.lastPoints.Dequeue();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class SnakeController : MonoBehaviour
+    {
+        [SerializeField] private Character defaultCharacter;
+        [SerializeField] private BodyPartItem defaultBodyPart;
+        [SerializeField] private float spaceBetweenPart;
+
+        public GameObject bodyPartPrefab;
+        public List<BodyPartItem> BodyParts;
+
+        public void Awake()
+        {
+            BodyParts = new List<BodyPartItem>();
+            defaultBodyPart.Initialize(defaultCharacter, null);
+            BodyParts.Add(defaultBodyPart);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.BackQuote))
+            {
+                AddBodyPart(defaultCharacter);
+            }
+        }
+
+        private void AddBodyPart(Character character)
+        {
+            int lastIndex = BodyParts.Count - 1;
+            var lastBodyPartTransform = BodyParts[lastIndex].transform;
+            Vector3 point = lastBodyPartTransform.position + -lastBodyPartTransform.forward * spaceBetweenPart;
+
+            var bodyPartObject = Instantiate(bodyPartPrefab, point, lastBodyPartTransform.rotation);
+            bodyPartObject.transform.SetParent(this.transform);
+            var bodyPart = bodyPartObject.GetComponent<BodyPartItem>();
+
+            bodyPart.Initialize(character, BodyParts[lastIndex]);
+            BodyParts.Add(bodyPart);
+        }
+    }
+}
