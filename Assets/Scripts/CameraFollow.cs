43a1b7e12dfcb15a4dc0fb40d4b72aa5a3bd227a
tiyago1 <tiyago1@gmail.com>
2023-04-08 18:50:02 +0300

    camera follow added

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform player;
+        public float smoothTime = 0.3f;
+        public float zOffset = 0.0f;
+
+        private Vector3 velocity = Vector3.zero;
+
+        private void Update()
+        {
+            Vector3 targetPosition = new Vector3(transform.position.x, transform.position.y, player.position.z + zOffset);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+}
