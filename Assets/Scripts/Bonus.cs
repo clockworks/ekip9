fd01d1d989e8116e3159c22d6cde8178cbcf679d
Melih T. Tolan <melih.t.tolan@gmail.com>
2023-04-08 17:02:25 +0300

    Test Sphere in case of collusion

--- Assets/Scripts/Bonus.cs
+++ Assets/Scripts/Bonus.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Bonus : MonoBehaviour
 {
+
+    void OnTriggerEnter(Collider other) 
+    {
+        Debug.Log("OnTriggerEnter");
+        
+        if (other.gameObject.tag=="Player")
+        {
+            //If gameobject has tag "BuyPottery", than destory it.
+            Destroy(other.gameObject);
+        }
+    }
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Update");
     }
 }
