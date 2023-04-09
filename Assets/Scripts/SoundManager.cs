e27491686852d5a95c7d7bf038141a60010ce734
tiyago1 <tiyago1@gmail.com>
2023-04-09 17:39:01 +0300

    music added

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public AudioClip Music;
+        public AudioSource Source;
+
+        public Image SoundButtonImage;
+        public Sprite OpenSprite;
+        public Sprite CloseSprite;
+        private bool isMusicActive = true;
+
+        public void OnSoundButtonClicked()
+        {
+            isMusicActive = !isMusicActive;
+            Sprite sprite = isMusicActive ? OpenSprite : CloseSprite;
+            SoundButtonImage.sprite = sprite;
+
+            if (!isMusicActive)
+            {
+                Source.Pause();
+            }
+            else
+            {
+                Source.Play();
+            }
+        }
+    }
+}
