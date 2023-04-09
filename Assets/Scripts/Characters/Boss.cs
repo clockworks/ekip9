4e04c06d2d8610f11ae27ad7b902d9623352730d
tiyago1 <tiyago1@gmail.com>
2023-04-09 16:06:32 +0300

    boss added

---
+++ Assets/Scripts/Characters/Boss.cs
@@ -0,0 +1,33 @@
+﻿using DefaultNamespace.TurnBasedGame;
+using DG.Tweening;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Boss : Character
+    {
+        public override void Attack()
+        {
+            base.Attack();
+            Animator.SetTrigger("Move");
+            Model.transform.DOMove(TargetCharacter.AttackPoint.position, 1)
+                .OnComplete(() =>
+                    {
+                        Animator.SetTrigger("Attack");
+                        StartCoroutine(GameManager.Instance.WaitAndExecute(() =>
+                                                                           {
+                                                                               TargetCharacter.TakeDamage(Damage);
+                                                                               if (IsPlayer)
+                                                                               {
+                                                                                   AttackButton.interactable = false;
+                                                                               }
+                                                                               Model.transform.localPosition = Vector3.zero;
+                                                                           },
+                                                                           2.5f
+                                       )
+                        );
+                    }
+                );
+        }
+    }
+}
